fcb2ab36a496a44ec9450eb5fc5050c0f863dcd7
piyomogi1293 <yomoyomo1293@gmail.com>
2023-12-30 17:57:21 +0900

    useMediaQuery.tsxを削除

--- src/component/RadioButtonGroup.tsx
+++ src/component/RadioButtonGroup.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import RadioButton from './RadioButton';
-import { mediaQuery, useMediaQuery } from './useMediaQuery';
 
 interface RadioButtonGroupProps {
     options: { label: string; value: string }[];
@@ -17,45 +16,14 @@ const Styles: { [key: string]: React.CSSProperties } = {
         justifySelf: "auto",
         marginLeft: "10px"
     },
-    radio_list_m: {
-        flexWrap: "wrap",
-        justifyContent: "flex-start",
-        justifySelf: "auto",
-        marginLeft: "10px"
-    }
 };
 
 const RadioButtonGroup: React.FC<RadioButtonGroupProps> = ({ options, value, onChange }) => {
 
-    // レスポンシブ対応
-    const isSp = useMediaQuery(mediaQuery.sp);
-
-    if (isSp) {
-        return (
-            <>
-                <p style={Styles.text}> 表示するデータを選んでください。 なお、選択を変更した後はもう一度都道府県を選び直してください。</p>
-                <div style={Styles.radioList_m}>
-                    {options.map((option) => (
-                        <div key={option.value} style={Styles.radioList}>
-                            <RadioButton
-                                key={option.value}
-                                label={option.label}
-                                value={option.value}
-                                checked={value === option.value}
-                                onChange={onChange}
-                            />
-                        </div>
-                    ))}
-                    {/* <p>Selected Value: {value}</p> */}
-                </div>
-            </>
-        );
-    }
-
     return (
         <>
             <p style={Styles.text}> 表示するデータを選んでください。 なお、選択を変更した後はもう一度都道府県を選び直してください。</p>
-            <div style={Styles.radioList_b}>
+            <div style={Styles.radioList}>
                 {options.map((option) => (
                     <div key={option.value} style={Styles.radioList}>
                         <RadioButton
